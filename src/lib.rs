271ba1fc8348ecb5489e18b57de0b95414fffea8
ozyx <jessemazzella@gmail.com>
2020-01-30 16:07:23 -0800

    use enums for call types

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,18 @@ use reqwest::Error;
 use serde::Deserialize;
 use std::time::Duration;
 
-// TODO: This probably shouldn't be entirely public
 #[derive(Deserialize, Debug)]
 pub struct Deck {
-    pub success: bool,
-    pub deck_id: String,
-    pub remaining: i32,
-    pub cards: Vec<Card>
+    success: bool,
+    deck_id: String,
+    remaining: i32,
+    cards: Vec<Card>,
+}
+
+impl Deck {
+    pub fn get_deck_id(self) -> String {
+        return self.deck_id.clone();
+    }
 }
 
 #[derive(Deserialize, Debug)]
@@ -23,57 +28,75 @@ pub struct Card {
     code: String,
 }
 
-#[derive(Deserialize, Debug)]
-pub struct DrawCard {
-    pub deck_id: Option<String>,
-    pub count: u16,
+impl Card {
+    pub fn get_image(self) -> String {
+        return self.image;
+    }
+    pub fn get_value(self) -> String {
+        return self.value;
+    }
+    pub fn get_suit(self) -> String {
+        return self.suit;
+    }
+    pub fn get_code(self) -> String {
+        return self.code;
+    }
 }
 
-impl RestPath<DrawCard> for Deck {
-    fn get_path(params: DrawCard) -> Result<String, Error> {
+pub enum DeckOfCardsActions {
+    DrawCard(Option<String>, u16),
+    NewDeck,
+    ShuffleDeck(String),
+}
 
-        match params.deck_id {
-            Some(id) => Ok(format!(
-                "https://deckofcardsapi.com/api/deck/{0}/draw/?count={1}",
-                id, params.count
-            )),
-            None => Ok(format!(
-                "https://deckofcardsapi.com/api/deck/new/draw/?count={0}",
-                params.count
-            )),
+impl RestPath<DeckOfCardsActions> for Deck {
+    fn get_path(params: DeckOfCardsActions) -> Result<String, Error> {
+        use DeckOfCardsActions::{DrawCard, NewDeck, ShuffleDeck};
+        match params {
+            DrawCard(deck_id, count) => match deck_id {
+                Some(id) => Ok(format!("/api/deck/{0}/draw/?count={1}", id, count)),
+                None => Ok(format!("/api/deck/new/draw/?count={0}", count)),
+            },
+            NewDeck => Ok("/api/deck/new/".to_string()),
+            ShuffleDeck(deck_id) => Ok(format!("/api/deck/{0}/shuffle/", deck_id)),
         }
     }
 }
 
-pub trait CardApi {
-    fn base_url() -> &'static str {
-        "https://deckofcardsapi.com"
-    }
-}
+//---------------------------------------------------------------------------
 
 pub trait RestPath<U> {
     fn get_path(params: U) -> Result<String, Error>;
 }
 
 pub struct RestClient {
+    base_url: String,
     client: reqwest::blocking::Client,
 }
 
 impl RestClient {
-    pub fn new() -> Result<RestClient, Error> {
-        Ok(RestClient {
+    pub fn new(base_url: String) -> Result<RestClient, Error> {
+        let client = RestClient {
+            base_url: base_url,
             client: reqwest::blocking::Client::builder()
                 .timeout(Duration::from_secs(10))
                 .build()?,
-        })
+        };
+
+        Ok(client)
     }
 
     pub fn get_sync<U, T>(&mut self, params: U) -> Result<T, Error>
     where
         T: serde::de::DeserializeOwned + RestPath<U>,
     {
-        let res = self.client.get(T::get_path(params)?.as_str()).send()?;
+        let path = T::get_path(params)?;
+        let res = self
+            .client
+            .get((self.base_url.clone() + &path).as_str())
+            .send()?;
         let json = res.json()?;
+
         Ok(json)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 use cardbot::*;
 
 fn main() {
-    let mut client = RestClient::new().unwrap();
+    let mut client = RestClient::new("https://deckofcardsapi.com".to_string()).expect("Something went egregiously wrong?");
+
     let data: Deck = client
-        .get_sync(DrawCard {
-            deck_id: Option::None,
-            count: 1,
-        })
+        .get_sync(DeckOfCardsActions::DrawCard(Option::None, 1))
         .unwrap();
-    println!("{:?}", data);
-    let data2: Deck = client.get_sync(DrawCard {
-        deck_id: Some(data.deck_id),
-        count: 5
-    }).unwrap();
-    println!("{:?}", data2);
+
+    println!("{:#?}", data);
+
+    let data2: Deck = client
+        .get_sync(DeckOfCardsActions::DrawCard(Some(data.get_deck_id()), 5))
+        .unwrap();
+
+    println!("{:#?}", data2);
 }
